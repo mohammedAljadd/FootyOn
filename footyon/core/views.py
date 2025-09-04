a5a592884524682047bb75ab0ce592314c08af9b
mohammedAljadd <aljadd3@gmail.com>
2025-09-04 05:35:15 +0200

    feat:show upco matches

--- footyon/core/views.py
+++ footyon/core/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from datetime import date
 from matches.models import Match
 from participation.models import Participation
 
 def home(request):
     """
-    Home page view
+    Home page view: shows upcoming matches and Join/Leave buttons
     """
 
-    return render(request, 'home.html')
+    upcoming_matches = Match.objects.filter(date__gte=date.today()).order_by('date', 'time')
+    
+    context = {
+        'upcoming_matches': upcoming_matches,
+    }
+    return render(request, 'home.html', context)
