17bd796c2fe6728b27c0090595c7d0583f59deb9
mohammedAljadd <aljadd3@gmail.com>
2025-09-04 05:16:29 +0200

    app namepsace helper

--- footyon/matches/urls.py
+++ footyon/matches/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+# This sets a namespace for this app's URLs.
+# Using app_name allows us to refer to these URLs unambiguously in templates or views
+# Example: {% url 'matches:manage' %} or redirect('matches:create_match')
 app_name = 'matches'
 
 urlpatterns = [
